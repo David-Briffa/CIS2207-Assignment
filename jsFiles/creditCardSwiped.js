c24b8aac15e8fca1af7ff1db0e4d9cd866879fa7
David-Briffa <94389122+David-Briffa@users.noreply.github.com>
2022-11-20 10:52:54 +0100

    Cookie functions

--- jsFiles/creditCardSwiped.js
+++ jsFiles/creditCardSwiped.js
@@ -1,5 +1,7 @@
 
 var information = document.getElementById("information");  
+var payByCard = document.cookie;
+var text = document.getElementById("text");
 
 function Swipe() {
     localStorage.setItem('swiped', 'True');
@@ -10,6 +12,7 @@ function Swipe() {
  function Accepted() {
     if(localStorage.getItem('swiped') == 'True'){
         localStorage.removeItem('swiped');
+        document.cookie = 'cardAccepted=True';
         window.location.replace("fuelDisplay.html");
     }
  }
@@ -19,5 +22,32 @@ function Swipe() {
        }
  
  function PayByCard(){
-    localStorage.setItem('card', 'True');
+    document.cookie = 'payByCard=True';
  }
+ function getCookie(cname) {
+   let name = cname + "=";
+     let ca = document.cookie.split(';');
+     for(let i = 0; i < ca.length; i++) {
+       let c = ca[i];
+       while (c.charAt(0) == ' ') {
+  c = c.substring(1);
+   }
+   if (c.indexOf(name) == 0) {
+         return c.substring(name.length, c.length);
+ }
+   }
+     return "";
+   }
+ function checkCardCookie() {
+   var bool = getCookie("payByCard");
+     if (bool != "") {
+      alert("cookie is set, this transaction must lead to card interface after amount is set");
+     }
+   }
+   function checkCardPumpCookie() {
+      var bool = getCookie("cardAccepted");
+        if (bool != "") {
+         alert("cookie is set, can pump up to 120eu");
+        }
+      }
+ 
