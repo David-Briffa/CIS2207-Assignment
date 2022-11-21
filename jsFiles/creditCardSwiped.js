0dbe9338bf6c8cd13fdbeede5850394abf5461ac
David-Briffa <94389122+David-Briffa@users.noreply.github.com>
2022-11-21 17:54:37 +0100

    cleaned code
    needs cookie rework

--- jsFiles/creditCardSwiped.js
+++ jsFiles/creditCardSwiped.js
@@ -1,13 +1,11 @@
-
 var information = document.getElementById("information");  
 var payByCard = document.cookie;
 var text = document.getElementById("text");
 
 function Swipe() {
     localStorage.setItem('swiped', 'True');
-
     information.textContent = "Card Has Been Swiped";       
-    document.body.appendChild(information);   
+    information.body.appendChild(information);   
  }
  function Accepted() {
     if(localStorage.getItem('swiped') == 'True'){
@@ -18,7 +16,7 @@ function Swipe() {
  }
  function Denied() {
         information.textContent = "Card has been denied, please swipe again";       
-        document.body.appendChild(information);    
+        information.body.appendChild(information);    
        }
  
  function PayByCard(){
@@ -26,22 +24,24 @@ function Swipe() {
  }
  function getCookie(cname) {
    let name = cname + "=";
-     let ca = document.cookie.split(';');
+   let ca = document.cookie.split(';');
      for(let i = 0; i < ca.length; i++) {
        let c = ca[i];
-       while (c.charAt(0) == ' ') {
-  c = c.substring(1);
-   }
-   if (c.indexOf(name) == 0) {
+       while (c.charAt(0) == ' ') 
+       {
+           c = c.substring(1);
+       }
+         if (c.indexOf(name) == 0) 
+       {     
          return c.substring(name.length, c.length);
- }
+       }
    }
      return "";
    }
  function checkCardCookie() {
    var bool = getCookie("payByCard");
      if (bool != "") {
-      alert("cookie is set, this transaction must lead to card interface after amount is set");
+       alert("cookie is set, this transaction must lead to card interface after amount is set");
      }
    }
    function checkCardPumpCookie() {
