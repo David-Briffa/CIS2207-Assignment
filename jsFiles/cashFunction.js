6df1609537dda11556c381538e7441060d1ea1b2
Olesia Shtanko <olesia.shtanko.21@um.edu.mt>
2022-11-29 19:47:20 +0100

    added 5 euro note

--- jsFiles/cashFunction.js
+++ jsFiles/cashFunction.js
@@ -1,10 +1,34 @@
+const fiveEuro = document.getElementById('5euro_button');
 const tenEuro = document.getElementById('10euro_button');
 const twentyEuro = document.getElementById('20euro_button');
 const fiftyEuro = document.getElementById('50euro_button');
 const Total = document.getElementById('totalsale');
 
 var currentResult = 0;
+var payByCash = document.cookie;
 
+//sets cookie expiry date
+var date = new Date();
+date.setTime(date.getTime()+(10*365*24*60*60*1000));
+expires = " expires="+date.toUTCString();
+
+//set total amount
+function setTotal(total){
+    document.cookie = "TotalPay=" + total + ";" + expires + ";";
+}
+
+/*function setLocalS(){
+    localStorage.setItem('test', 1);
+}*/
+
+//adds 5 to total
+function addFive(){
+    const value = parseInt(fiveEuro.value);
+    currentResult += value;
+    outputTotal(currentResult);
+}
+
+//adds 10 to total
 function addTen(){
 
     const value = parseInt(tenEuro.value);
@@ -12,6 +36,7 @@ function addTen(){
     outputTotal(currentResult);
 }
 
+//adds 20 to total
 function addTwenty(){
 
     const value = parseInt(twentyEuro.value);
@@ -19,6 +44,7 @@ function addTwenty(){
     outputTotal(currentResult);
 }
 
+//adds 50 to total
 function addFifty(){
 
     const value = parseInt(fiftyEuro.value);
@@ -26,10 +52,8 @@ function addFifty(){
     outputTotal(currentResult);
 }
 
+//outputs total
 function outputTotal(total){
-    Total.textContent = total;
-    var totalObject = {value : total};
-    var totalObjectString = JSON.stringify(totalObject);
-    localStorage.setItem('total', totalObjectString);
+    Total.textContent = total;    
 }
 
