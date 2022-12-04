c39f7071259ebb937ef2689c0453a8d61f8dedf5
David-Briffa <94389122+David-Briffa@users.noreply.github.com>
2022-12-04 17:13:20 +0100

    undo for cash.html

--- jsFiles/undoAndCancel.js
+++ jsFiles/undoAndCancel.js
@@ -2,3 +2,8 @@ function cancelTransaction(){
     sessionStorage.clear();
     window.location.replace("index.html");
 }
+
+function cashUndo(){
+    sessionStorage.removeItem('payByCard');
+    window.location.replace("paymentInterface.html");
+}
