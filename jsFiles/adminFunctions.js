de2af25f9c66f2bb2d9548efa57fd363c2c47954
marjohnsaliba4 <103215379+marjohnsaliba4@users.noreply.github.com>
2022-12-07 17:20:37 +0100

    Add files via upload
    Last few bugs fixed

--- jsFiles/adminFunctions.js
+++ jsFiles/adminFunctions.js
@@ -14,7 +14,7 @@ function priceIntializeSetter(){
     //if it is the first start up then set the fuelPriceCookie
     if(localStorage.priceSetCheck!="1"){
 
-        localStorage.setItem("fuelPriceCookie","Diesel,1.21,PetrolRegular,1.50,PetrolHi-grade,1.30;");
+        localStorage.setItem("fuelPriceCookie","Diesel,1.21,PetrolRegular,1.30,PetrolHi-grade,1.50");
 
     }
 
