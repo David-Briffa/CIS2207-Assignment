0d29c63c241804080c3ef775be4bd6ef42f3c776
Olesia Shtanko <olesia.shtanko.21@um.edu.mt>
2022-12-12 00:25:42 +0100

    small button changes

--- jsFiles/creditCardSwiped.js
+++ jsFiles/creditCardSwiped.js
@@ -1,46 +1,61 @@
-var information = document.getElementById("information");  
+var information = document.getElementById("information");
 var payByCard = document.cookie;
 var text = document.getElementById("text");
 
-function Swipe() {
-    localStorage.setItem('swiped', 'True');
-    information.textContent = "Card Has Been Swiped";       
-    information.body.appendChild(information);   
- }
- function Accepted() {
-    if(localStorage.getItem('swiped') == 'True'){
-        localStorage.removeItem('swiped');
-        document.cookie = 'cardAccepted=True';
-        window.location.replace("fuelDisplay.html");
-    }
- }
- function Denied() {
-    information.textContent = "Card has been denied, please swipe again";       
-    information.body.appendChild(information);    
-       }
- 
- function PayDynamically(){
-    sessionStorage.setItem('payDynamically', 'True');
-}
- 
- function CheckDynamic(){
-    if(sessionStorage.getItem('payDynamically') == 'True'){
-      alert("This user can buy up to 120eu worth of petrol");
-  }
- }
+const swipe = document.getElementById("swipe");
+const animation = document.getElementById("animation");
+const accepted = document.getElementById("accepted_button");
+const denied = document.getElementById("denied_button");
 
- function PayByCard(){
-     sessionStorage.setItem('payByCard', 'True');
+animation.style.display = "none";
+denied.style.display = "none";
+accepted.style.display = "none";
+
+function Swipe() {
+  swipe.style.display = "none";
+  animation.style.display = "block";
+  denied.style.display = "block";
+  accepted.style.display = "block";
+
+  localStorage.setItem("swiped", "True");
+  information.textContent = "Card Has Been Swiped";
+  information.body.appendChild(information);
+}
+function Accepted() {
+  if (localStorage.getItem("swiped") == "True") {
+    localStorage.removeItem("swiped");
+    document.cookie = "cardAccepted=True";
+    window.location.replace("fuelDisplay.html");
   }
+}
+function Denied() {
+  swipe.style.display = "block";
+  animation.style.display = "none";
+  denied.style.display = "none";
+  accepted.style.display = "none";
   
- function CheckCard(){
-    if(sessionStorage.getItem('payByCard') == 'True')
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-    }
- 
+  information.textContent = "Card has been denied, please swipe again";
+  information.body.appendChild(information);
+}
+
+function PayDynamically() {
+  sessionStorage.setItem("payDynamically", "True");
+}
+
+function CheckDynamic() {
+  if (sessionStorage.getItem("payDynamically") == "True") {
+    alert("This user can buy up to 120eu worth of petrol");
+  }
+}
+
+function PayByCard() {
+  sessionStorage.setItem("payByCard", "True");
+}
+
+function CheckCard() {
+  if (sessionStorage.getItem("payByCard") == "True") {
+    return true;
+  } else {
+    return false;
+  }
+}
