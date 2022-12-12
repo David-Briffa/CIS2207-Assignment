dc859114cdb89ab34f161745fd6100049a014cf8
David Briffa <david.briffa.21@um.edu.mt>
2022-12-12 08:17:38 +0100

    Revert "Revert "Merge branch 'Third-Prototype' of https://github.com/David-Briffa/CIS2207-Assignment into Third-Prototype""
    This reverts commit a6e686c47374df046548d71c2c020777bfa8db25.

--- jsFiles/creditCardSwiped.js
+++ jsFiles/creditCardSwiped.js
@@ -1,6 +1,15 @@
-var information = document.getElementById("information");  
+var information = document.getElementById("information");
 var payByCard = document.cookie;
 var text = document.getElementById("text");
+
+const swipe = document.getElementById("swipe");
+const animation = document.getElementById("animation");
+const accepted = document.getElementById("accepted_button");
+const denied = document.getElementById("denied_button");
+
+animation.style.display = "none";
+denied.style.display = "none";
+accepted.style.display = "none";
 
 function Swipe() {
     localStorage.setItem('swiped', 'True');
@@ -14,17 +23,6 @@ function Swipe() {
         window.location.replace("fuelDisplay.html");
     }
  }
- function transactionComplete(){
-   if(sessionStorage.getItem('payDynamically2') == 'True' || sessionStorage.getItem('payByCard')== 'True'){
-      information.textContent = "Card has been charged successfully.";
-      information.body.appendChild(information);  
-   }
-   else{
-      information.textContent = "Thank you for using our station!";
-      information.body.appendChild(information);  
-   }
-     
- }
  function Denied() {
     information.textContent = "Card has been denied, please swipe again";       
     information.body.appendChild(information);    
@@ -32,27 +30,22 @@ function Swipe() {
  
  function PayDynamically(){
     sessionStorage.setItem('payDynamically', 'True');
-    sessionStorage.setItem('payDynamically2', 'True');
 }
- 
- function CheckDynamic(){
-    if(sessionStorage.getItem('payDynamically') == 'True'){
-      alert("This user can buy up to 120eu worth of petrol");
-  }
- }
 
- function PayByCard(){
-     sessionStorage.setItem('payByCard', 'True');
+function CheckDynamic() {
+  if (sessionStorage.getItem("payDynamically") == "True") {
+    alert("This user can buy up to 120eu worth of petrol");
   }
-  
- function CheckCard(){
-    if(sessionStorage.getItem('payByCard') == 'True')
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-    }
- 
+}
+
+function PayByCard() {
+  sessionStorage.setItem("payByCard", "True");
+}
+
+function CheckCard() {
+  if (sessionStorage.getItem("payByCard") == "True") {
+    return true;
+  } else {
+    return false;
+  }
+}
